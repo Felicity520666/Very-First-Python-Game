4de2e9974690cee7fe4d9bd1718baeefe9b3640f
Felicity520666 <yyan520666@gmail.com>
2025-12-10 09:03:12 -0500

    changed bg and did more

--- game.py
+++ game.py
@@ -4,22 +4,45 @@ import random
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Dodge")
+pygame.display.set_caption("NO HOMEWORK!!!")
 
-BG = pygame.image.load("assets/bg.jpg")
+BG = pygame.transform.scale(pygame.image.load("assets/bg.jpg"), (WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+PLAYER_VEL = 5
+
+def draw(player):
     WIN.blit(BG, (0,0))
+
+    pygame.draw.rect(WIN, "pink", player)
+   
     pygame.display.update()
 
 def main ():
     run = True
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+        
+        draw(player)
+    
     pygame.quit()
 
 if __name__ == "__main__":
